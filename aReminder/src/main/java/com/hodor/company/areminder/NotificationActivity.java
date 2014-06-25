b1883f886554def33d3729050b903af8dc1b1647
Adam Anh Doan Kim Caramés <dkc.adam@gmail.com>
2014-06-26 00:32:30 +0200

    Licensing added

--- aReminder/src/main/java/com/hodor/company/areminder/NotificationActivity.java
+++ aReminder/src/main/java/com/hodor/company/areminder/NotificationActivity.java
@@ -1,10 +1,27 @@
+/**
+    aReminder - an Android + Google wear application test for I/O 2014
+
+    Copyright (C) 2014  Toni Martinez / Adam Doan Kim
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package com.hodor.company.areminder;
 
 import android.app.Activity;
 
-/**
- * Created by adoankim on 25/06/14.
- */
 public class NotificationActivity extends Activity {
     @Override
     protected void onStart() {
--- aReminder/src/main/java/com/hodor/company/areminder/RemindService.java
+++ aReminder/src/main/java/com/hodor/company/areminder/RemindService.java
@@ -1,3 +1,24 @@
+/**
+    aReminder - an Android + Google wear application test for I/O 2014
+
+    Copyright (C) 2014  Toni Martinez / Adam Doan Kim
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
 package com.hodor.company.areminder;
 
 import android.app.AlarmManager;
@@ -10,9 +31,6 @@ import android.preview.support.wearable.notifications.WearableNotifications;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-/**
- * Created by adoankim on 25/06/14.
- */
 public class RemindService extends Service {
 
     private int time;
